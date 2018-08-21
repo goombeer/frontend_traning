a863c7b13d379c9b13dac3f99f91984819e8fb3f
goombeer <highbridge71@gmail.com>
2018-08-22 00:05:40 +0900

    learned prototype

---
+++ app.js
@@ -0,0 +1,12 @@
+function Human(name){
+    this.name = name;
+}
+
+Human.prototype.greet = function(){
+    console.log("Hello" + this.name);
+}
+var alice = new Human("Alice");
+alice.greet()
+
+var bob = new Human("Bob");
+bob.greet();
