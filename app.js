85f42262cf5b552928f02de00e1b84630cea9f25
goombeer <highbridge71@gmail.com>
2018-08-27 00:01:49 +0900

    leraned Observer

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-function createCounter(){
-    var count = 0;
-    return function(){
-        count++;
-        console.log(count);
+function Observer(){
+    this.listeners = [];
+}
+
+Observer.prototype.on = function(func){
+    this.listeners.push(func)
+}
+
+Observer.prototype.off = function(func){
+    var len = this.listeners.length;
+    for (var i = 0; i < len; i++){
+        var listener = this.listeners[i];
+        if (listener === func){
+            this.listeners.splice(i,1);
+        }
+    }
+};
+
+Observer.prototype.trigger = function(){
+    var len = this.listeners.length;
+    for (var i = 0; i < len; i++){
+        var listener = this.listeners[i];
+        listener();
     }
 }
 
-var counter1 = createCounter();
-counter1();
-counter1();
-counter1();
+var observer = new Observer();
+var greet = function(){
+    console.log('Good morning');
+}
 
-var counter2 = createCounter();
-counter2();
-counter2();
+var gree = function(){
+    console.log('test');
+}
 
-count = 100;
-
-counter1();
+observer.on(greet);
+observer.on(gree);
+observer.trigger();
